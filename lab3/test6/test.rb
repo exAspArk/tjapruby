070ca1c86a4aad3ce57bb0cec70902ba1fca0bb5
exAspArk <exAspArk@exAspArk-PC>
2011-12-29 15:41:17 +0300

    ?????????? ?????, ??????? - ????? ?????????? ? ???? ??????. ??????? test6 ?? ???? ????????????

---
+++ lab3/test6/test.rb
@@ -0,0 +1,16 @@
+class Cycle
+	def omg
+		for i in list do a = b + c, if a == b then a += 1, b += 2 else a = b end, while a > 3 do a = 4, b = 3 end end
+	end
+	def omgg
+		for i in lst
+			if a == c
+				a += 3
+				c = 4
+			else
+				c = 5
+				a = c
+			end
+		end
+	end
+end
